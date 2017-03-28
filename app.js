29e0c7e1286a300fda3b7a101d91a5c21fd08d1b
Zak.zhang <zak.zhang@qq.com>
2017-03-28 18:15:03 +0800

    X

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    wx.login({
+        success: function (res) {
+          console.log(res)
+          wx.request({
+            url: 'https://api.xiaofengketang.com/jersey/wx/jscode2session', //仅为示例，并非真实的接口地址
+            data: {
+              js_code: res.code
+            },
+            header: {
+                'content-type': 'application/json'
+            },
+            method:"GET",
+            success: function(ress) {
+              console.log(ress)
+            }
+          })
+        }
+      })
   },
   getUserInfo:function(cb){
     var that = this
@@ -16,6 +34,7 @@ App({
         success: function () {
           wx.getUserInfo({
             success: function (res) {
+              console.log(res)
               that.globalData.userInfo = res.userInfo
               typeof cb == "function" && cb(that.globalData.userInfo)
             }
